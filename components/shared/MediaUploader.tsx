3fb5e9e768dff45569cd94f919b72f52b73f9594
oumar3900 <160543376+oumar3900@users.noreply.github.com>
2024-03-23 15:36:22 +0100

    Update MediaUploader.tsx

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -53,7 +53,7 @@ const MediaUploader = ({
 
   return (
     <CldUploadWidget
-      uploadPreset="jsm_imaginify"
+      uploadPreset="digiflex_onyx"
       options={{
         multiple: false,
         resourceType: "image",
